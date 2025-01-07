ab24bf56973c4c61fd11da258493f0598224d16a
parkyoungjin-888 <pyj8813@gmail.com>
2025-01-07 22:24:20 +0900

    analysis_config_model 추가 : data_model_module

---
+++ data_model_module/data_model_module/beanie_data_model/analysis_config_model.py
@@ -0,0 +1,38 @@
+from enum import Enum
+from typing import Optional
+from beanie import PydanticObjectId
+from pydantic import BaseModel, Field, root_validator
+
+
+class AnalysisCategory(Enum):
+    OCR = 'ocr'
+    OBJECT_DETECTION = 'object_detection'
+
+
+class AnalysisConfig(BaseModel):
+    name: str
+    img_collection: Optional[str | None] = None
+    category: Optional[AnalysisCategory | None] = None
+    config: dict = None
+
+    class Config:
+        extra = 'allow'
+
+    @root_validator(pre=True)
+    def type_based_conversion(cls, values):
+        for field_name, value in values.items():
+            if field_name not in cls.__annotations__:
+                continue
+            field_type = cls.__annotations__.get(field_name)
+            if field_type == str and not isinstance(value, str):
+                values[field_name] = str(value)
+            if field_type == int and isinstance(value, str) and value.isdigit():
+                values[field_name] = int(value)
+            elif field_type == int and isinstance(value, float):
+                values[field_name] = int(value)
+            elif field_type == float and isinstance(value, str):
+                try:
+                    values[field_name] = float(value)
+                except ValueError:
+                    raise ValueError(f'Field {field_name} expects a float value, got {value}')
+        return values
